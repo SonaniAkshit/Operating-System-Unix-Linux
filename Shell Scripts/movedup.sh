e8357b4c447ff5b264559a731cbaed1f524869be
SonaniAkshit <sonaniakshit777@gmail.com>
2025-02-19 16:03:43 +0530

    commit

--- Shell Scripts/movedup.sh
+++ Shell Scripts/movedup.sh
@@ -1,12 +1,53 @@
+# Program 29.
+#############
 # write a shell script to copy a duplicate file from two directory to third
-# directory and remove both the old files. Pass names of the directory as
-# command line argument.
+# directory and remove both the old files.
 
-read -p 'first file: ' f1
-read -p 'sec file: ' f2
+dir=$1
+while true;do
 
-if cmp -s "$f1" "$f2"; then
-    echo "same"
-fi
+	echo ..........MENU..........
+	echo 1.Operation on PWD.
+	echo 2.Operation on Selected Path.
+	echo 3.exit
+	echo ........................
+	echo ' '
 
+	read -p 'Enter any one choice: ' ch
+	echo ' '
 
+	if [ $ch -eq 1 ];then
+		read -p 'Enter name of first file: ' f1
+		read -p 'Enter name of Second file: ' f2
+		
+		if cmp -s $f1 $f2
+		then
+
+			echo $f1 and $f2 are same files,then...
+			cat $f1 > newdup
+			rm -v $f1 $f2
+			echo "Files $f1 and $f2 are remove and copy all contain in 'newdup' file."
+		else
+			echo Both files are diffrent..
+		fi
+
+	elif [ $ch -eq 2 ];then
+		read -p 'Enter Dir Path: ' dir
+		read -p 'Enter name of first file: ' f1
+		read -p 'Enter name of second file: ' f2
+		
+		file1 = $dir/$f1
+		file2 = $dir/$f2
+
+		if cmp -s $file1 $file2
+		then
+			echo same
+		else
+			echo not same
+		fi
+
+	elif [ $ch -eq 3 ];then
+		echo exit...
+		exit 1
+	fi
+done
--- Shell Scripts/showcal.sh
+++ Shell Scripts/showcal.sh
@@ -2,6 +2,12 @@
 # ##########
 # Write a shell script for printing calender for the month that entered as a char argument
 
-read -p "Enter the month [1-12]: " month
-read -p "Enter the Year: " year
-cal $month $year
+#read -p "Enter the month [1-12]: " month
+read -p "Enter month or year: " input
+
+if [ $input -eq 2025 ];then
+	cal
+else
+	cal $input
+fi
+#cal  $input
